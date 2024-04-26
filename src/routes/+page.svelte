a393582e33ec020e504e53d4de51200000831462
Mika Fischer <61615177+LuckyMika@users.noreply.github.com>
2024-04-26 19:50:15 +0200

    Update .gitignore

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
 	import { onMount } from "svelte";
+	import db from "$lib/db"
 
 	onMount(() => {
 		// Clientside javascript runs in here
 	})
+
+	async function handleClick() {
+		await db.user.create({
+			data: {
+				email: "jeff@jeffrey.com",
+				username: "Jeff",
+			}
+		});
+
+		console.log("Creating a user")
+	}
 </script>
 
 <h1>This will be forum trust me bro</h1>
+<button on:click={handleClick} >Add "jeff" to the database</button>
